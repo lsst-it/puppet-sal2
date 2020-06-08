7cbfff112edeb3182455c7d17857d919ad29ce0e
Andy Loftus <aloftus@gmail.com>
2020-06-08 17:33:38 -0500

    Update for new conda install process
    Merge ospl, idl, salobj classes into one.

--- README.md
+++ README.md
@@ -3,28 +3,16 @@
 Install SAL components
  
 ## Dependencies
-- [puppet/git_resource](https://forge.puppet.com/puppet/git_resource)
 - [puppet/miniconda](https://github.com/lsst-it/puppet-miniconda)
  
 ## Reference
 
-### class sal2::obj (
--  String $git_repo_url,
--  String $version,
-### class sal2::dds (
--  String $ospl_package_name,
-### class sal2::idl (
--  String               $git_repo_url,
--  Stdlib::Absolutepath $rpm_idl_dir,
--  String               $version,
 ### class sal2 (
--  Array  $exec_path,
--  String $install_path,
+-  String $install_dir,
+-  String $ospl_version,
+-  Hash   $ospl_yumrepo_data,
 -  Array  $required_os_pkgs,
-### class sal2::rpm (
--  Array  $packages,
--  String $ts_sal_version,
--  String $ts_xml_version,
--  Hash   $yum_repos,
- 
+-  String $ts_idl_version,
+-  String $ts_salobj_version,
+
 [REFERENCE.md](REFERENCE.md)
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,34 +1,95 @@
 # @summary Install and configure the LSST Service Abstraction Layer (SAL)
 #
-# Install and configure the LSST Service Abstraction Layer (SAL)
+# Install and configure the LSST Service Abstraction Layer (SAL).
 #
-# @param exec_path
-#        Set path for execs.
-#        Use for allowing custom 'python' and 'pip' to be found.
+# Includes:
+# - OpenSplice
+# - ts-dds
+# - ts_idl
+# - ts_salobj
 #
-# @param install_path
+# @param install_dir
 #        Directory under which everything will be installed.
 #
+# @param ospl_version
+#        Version of OpenSplice to install.
+#        Used by both yum installer (for OpenSplice)
+#        and conda installer (for ts-dds)
+#
+# @param ospl_yumrepo_data
+#        `baseurl` and `descr` are required. Any other valid
+#        Puppet yumrepo parameters are optional.
+#
+# @param ts_idl_version
+#        Full version string as needed by conda package installer
+#        Format: <IDLversion>_<XMLversion>
+#
+# @param ts_salobj_version
+#        Full version string as needed by conda package installer
+#
 # @param required_os_pkgs
-#        Other OS packages this module depends on
+#        Other OS packages required by SAL
 #
 # @example
 #   include sal2
 class sal2 (
-  Array  $exec_path,
-  String $install_path,
+  String $install_dir,
+  String $ospl_version,
+  Hash   $ospl_yumrepo_data,
   Array  $required_os_pkgs,
+  String $ts_idl_version,
+  String $ts_salobj_version,
 ) {
 
-  file { $install_path :
-    ensure => directory,
+  file { $install_dir :
+    ensure  => directory,
+      mode  => '0755',
+      owner => 'root',
   }
 
-  # Dependent packages
+
+  # Dependent OS packages
   ensure_packages( $required_os_pkgs )
 
-  include ::sal2::rpm
-  include ::sal2::dds
-  include ::sal2::idl
-  include ::sal2::obj
+
+  # Ensure OpenSplice is installed
+  yumrepo {
+    'lsst-ts':
+      * => $ospl_yumrepo_data,
+    ;
+    default:
+      gpgcheck       => 0,
+      failovermethod => 'priority',
+    ;
+  }
+  $ospl_pkg_list = [ "OpenSpliceDDS-${ospl_version}" ]
+  $ospl_pkg_defaults = { 'ensure' =>  'present' }
+  ensure_packages( $ospl_pkg_list, $ospl_pkg_defaults )
+
+
+  # ENV files
+  $params_sal = { ospl_version => $ospl_version, }
+  $params_salobj = { sal_install_dir    => $install_dir, }
+  file {
+    "${install_dir}/setup_SAL.env" :
+      content => epp( 'sal2/setup_SAL.env.epp', $params_sal ),
+    ;
+    "${install_dir}/setup_salobj.env" :
+      content => epp( 'sal2/setup_salobj.env.epp', $params_salobj ),
+    ;
+    default:
+      ensure => file,
+      mode   => '0444',
+      owner  => 'root',
+  }
+
+
+  # Ensure T&S conda packages
+  $ts_packages = [
+    "ts-dds==v${ospl_version}",
+    "ts-idl=${ts_idl_version}",
+    "ts-salobj=${ts_salobj_version}",
+  ]
+  miniconda::package { $ts_packages : }
+
 }
